6a31ea6c3ecfcbefa03197b7b706bf7745a84418
Andy Shu <andy@symbol.is>
2017-12-05 22:47:44 +0800

    Revise UI

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const numberOfPeople = 50
+const numberOfPeople = 10
 const initialFund = 100
+const transactionAmount = 10
 const ledger = Array(numberOfPeople).fill(initialFund)
+const frameDelay = 100
 
 function sleep(n) {
     return new Promise((resolve, reject) => {
@@ -8,33 +10,56 @@ function sleep(n) {
     })
 }
 
-function randomChoice(array) {
-    return array[Math.floor(Math.random() * array.length)]
+let heightMultiplier = 5
+
+function render(ledger, donor, recipient, day) {
+    if (donor !== null && recipient !== null) {
+        document.querySelector('.clock').innerHTML = `${day} day: ${donor}->${recipient}`
+    }
+    else {
+        document.querySelector('.clock').innerHTML = `${day} day`
+    }
+    const divs = [].slice.call(document.querySelectorAll('.canvas div'))
+    ledger.forEach((fund, index) => {
+        divs[index].style.height = fund / (initialFund * heightMultiplier) * 100 + '%'
+        divs[index].style.background = index === donor ? 'red' : index === recipient ? 'green' : 'black'
+    })
 }
 
-function render(ledger, clock) {
-    document.querySelector('.clock').innerHTML = clock
-    document.querySelector('.canvas').innerHTML = 
-        ledger
-        .sort((a, b) => a - b)
-        .map(fund => `
-            <div style="height: ${fund * 5}px" class="bar"></div>
-        `)
-        .join('')
-}
-
-let clock = 0
-
-async function main() {
-    while (clock < 10000) {
-        await sleep(10)
-        const donor = Math.floor(Math.random() * numberOfPeople)
-        const recipient = Math.floor(Math.random() * numberOfPeople)
-        ledger[donor] -= 1
-        ledger[recipient] += 1
-        render(ledger, clock)
-        clock += 1
+function initBars() {
+    const canvas = document.querySelector('.canvas')
+    for (let i = 0; i < numberOfPeople; i += 1) {
+        const div = document.createElement('div')
+        div.style.left = i * 9.5 + 'vw',
+        canvas.appendChild(div)
     }
 }
 
-main()
+let day = 1
+let paused = true
+function frame() {
+    let donor, recipient
+    while (donor === recipient) {
+        donor = Math.floor(Math.random() * numberOfPeople)
+        recipient = Math.floor(Math.random() * numberOfPeople)
+    }
+    ledger[donor] -= transactionAmount
+    ledger[recipient] += transactionAmount
+    day += 1
+    render(ledger, donor, recipient, day)
+    if (!paused) {
+        setTimeout(frame, frameDelay)
+    }
+}
+
+document.querySelector('.control .start').onclick = () => {
+    paused = false
+    frame()
+}
+
+document.querySelector('.control .pause').onclick = () => {
+    paused = true
+}
+
+initBars()
+render(ledger, null, null, day)
